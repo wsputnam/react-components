5afe1c6ba42a7d748b3c6aa580e9403e8be9f0d9
Your Name <williamputnamjr@gmail.com>
2017-12-13 12:18:50 -0800

    finished react components sprint

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
 // TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      // <ul>
+      //   {this.props.Items.map(Item =>
+          <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.Item}</li>
+        // )}
+      // </ul>
+    );
+  }
+
+  // return (
+  //   <ul>
+  //     <li onClick={onListItemClick}> {this.props.Item}</li>
+  //   </ul>
+  //   );
+}
+
+var onListItemClick = (event) => {
+  console.log('I got clicked');
+  console.log('this', this);
+};
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.Items.map(Item => 
+      <GroceryListItem Item={Item} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList Items={['Coffee', 'Eggs']} />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
